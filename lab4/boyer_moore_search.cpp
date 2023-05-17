1e244a3fdbded7e127b0261b75b3d5096b35e942
KlukaCross <Jktu332@yandex.ru>
2023-05-17 21:07:50 +0300

    Исправлен баг в Z-алгоритме

--- lab4/boyer_moore_search.cpp
+++ lab4/boyer_moore_search.cpp
@@ -105,7 +105,7 @@ std::vector<uint64_t> BoyerMooreSearch::ZFunction(const std::deque<Tchar>& s) {
     uint64_t l = 0, r = 0;
     for (uint64_t i = 0; i < n; ++i) {
         if (i <= r) {
-            z[i] = std::min(r-l, z[i-l]);
+            z[i] = std::min(r-i, z[i-l]);
         }
         while (i+z[i] < n && s[z[i]] == s[i+z[i]]) {
             ++z[i];
